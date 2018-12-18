ed80c93aad65553f4a0edadcee27f8f2c3d83c43
C45tr0 <46m2.b34v3r@gmail.com>
2018-12-18 00:08:36 -0700

    feat: set up file upload for s3 signed urls

--- manager/s3-uploader.go
+++ manager/s3-uploader.go
@@ -21,7 +21,12 @@
 package manager
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 	"time"
 )
 
@@ -33,6 +38,15 @@ type S3Uploader struct {
 	lastUpload time.Time
 	uploadSessionId string
 	uploadSessionSecret string
+}
+
+type GetSignedUrlForUploadRequest struct {
+	uploadSessionId string
+	uploadSessionSecret string
+}
+
+type GetSignedUrlForUploadResponse struct {
+	signedUrl string
 }
 
 func NewS3Uploader(uploadSessionId string, uploadSessionSecret string) S3Uploader {
@@ -59,9 +73,74 @@ func (u S3Uploader) Upload(file string) (bool, error) {
 
 	fmt.Printf("Upload url: %#v\n", url)
 
+	fileReader, err := os.Open(file)
+	if err != nil {
+		return false, err
+	}
+
+	err = u.uploadFile(url, fileReader)
+	if err != nil {
+		return false, err
+	}
+
 	return true, nil
 }
 
 func (u S3Uploader) getUploadUrl() (string, error) {
-	return "", nil
+	// Probally want to do something different
+	message := GetSignedUrlForUploadRequest{
+		u.uploadSessionId,
+		u.uploadSessionSecret,
+	}
+
+	b, err := json.Marshal(message)
+	if err != nil {
+		return "", err
+	}
+
+	timeout := time.Duration(5 * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+
+	resp, err := client.Post(
+		"https://api.stellarisinsights.com/v1/signed-url", // maybe this should include the uploadSessionId in the url?
+		"application/json",
+		bytes.NewBuffer(b),
+	)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	var result GetSignedUrlForUploadResponse
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Printf("%#v", result)
+
+	return result.signedUrl, nil
+}
+
+func (u S3Uploader) uploadFile(signedUrl string, file io.Reader) (error) {
+	timeout := time.Duration(5 * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+
+	req, err := http.NewRequest("PUT", signedUrl, file)
+    if err != nil {
+        return err
+	}
+
+	resp, err := client.Do(req)
+    if err != nil {
+        return err
+	}
+	
+	fmt.Printf("%#v", resp)
+
+	return nil
 }
